0976a0b6be57ac2cc880ded83b0d9c8707b9c74f
Mark Ledesma <mcledesma@gmail.com>
2015-09-04 01:44:46 -0700

    added user logins with openid

--- config.py
+++ config.py
@@ -13,4 +13,8 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'}
+    ]
+
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
